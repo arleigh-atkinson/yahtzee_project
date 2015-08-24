66930535d4e31340f85fd662d14879ed465e3a8e
arleigh-atkinson <arleigh.atkinson@gmail.com>
2015-08-24 17:44:01 -0600

    Almost done yahtzee project

--- yahtzee.rb
+++ yahtzee.rb
@@ -1,18 +1,25 @@
+require 'pry'
+
+
+
 class Player
   attr_accessor :name, :scorecard, :total_score
 
-  def initialize(name, score)
+  def initialize(name)
     @name = name
-    @scorecard = score
+    @scorecard = ScoreCard.new
     @total_score = 0
   end
 
-  def calculate_score
-
+  def self.calculate_score
+    # temp = ScoreCard.player_choice(Game.turn)
+    # @total_score += temp
   end
 end
 
 class ScoreCard
+
+  #attr_accessor :scorecard_options
 
   @scorecard = {
     upper_section: {
@@ -54,7 +61,7 @@ class ScoreCard
     }
   }
 
-  def display_options
+  def self.display_options
     puts """Scoring Options:
     Upper Section:
     option            description
@@ -73,47 +80,61 @@ class ScoreCard
     end
   end
 
-  def player_choice(user_array)
+  def self.player_choice(user_array)
+    display_options
     puts "Choose a scoring option: "
     choice = gets.chomp
     case choice
     when 'aces'
+      @scorecard_options[:upper_section].delete(:aces)
       upper_section_scoring(1, user_array)
+      #binding.pry
     when 'twos'
+      @scorecard_options[:upper_section].delete(:twos)
       upper_section_scoring(2, user_array)
     when 'threes'
+      @scorecard_options[:upper_section].delete(:threes)
       upper_section_scoring(3, user_array)
     when 'fours'
+      @scorecard_options[:upper_section].delete(:fours)
       upper_section_scoring(4, user_array)
     when 'fives'
+      @scorecard_options[:upper_section].delete(:fives)
       upper_section_scoring(5, user_array)
     when 'sixes'
+      @scorecard_options[:upper_section].delete(:sixes)
       upper_section_scoring(6, user_array)
     when 'three_of_a_kind'
+      @scorecard_options[:lower_section].delete(:twos)
       sum_die(user_array)
     when 'four_of_a_kind'
+      @scorecard_options[:lower_section].delete(:four_of_a_kind)
       sum_die(user_array)
     when 'full_house'
+      @scorecard_options[:lower_section].delete(:full_house)
       25
     when 'small_straight'
+      @scorecard_options[:lower_section].delete(:small_straight)
       30
     when 'large_straight'
+      @scorecard_options[:lower_section].delete(:large_straight)
       40
     when 'yahtzee'
+      @scorecard_options[:lower_section].delete(:yahtzee)
       50
     when 'chance'
+      @scorecard_options[:lower_section].delete(:chance)
       sum_die(user_array)
     end
   end
 
-  def upper_section_scoring(number, arr)
+  def self.upper_section_scoring(number, arr)
     arr.count(number) * number
   end
 
-  def sum_die(arr)
+  def self.sum_die(arr)
     arr.inject(:+)
   end
-
 end
 
 class Game
@@ -154,4 +175,37 @@ end
 
 
 ## MAIN PROGRAM ##
-main = Game.turn
+puts 'Welcome to ~Yahtzee~'
+loop do
+  puts 'Would you like to play Yahtzee? 1: Yes 2: No'
+  play_yahtzee = gets.chomp
+  break if play_yahtzee == '2'
+  if play_yahtzee == '1'
+    player_hash = {}
+    puts 'How many are playing?'
+    number_of_players = gets.chomp.to_i
+    # loop through to get array of player names
+    number_of_players.times do 
+      print 'Please type your name: '
+      player_name = gets.chomp
+      player_hash[player_name] = Player.new(player_name)
+    end
+
+    # loop through turns
+    turns_taken = 0
+    while number_of_players*13 - turns_taken > 0
+      player_hash.each_pair do |key, value|
+        temp = ScoreCard.player_choice(Game.turn)
+        player_hash[key].total_score += temp
+        puts "#{temp} points! Total score: ", player_hash[key].total_score
+        binding.pry
+      end  
+      turns_taken += 1  
+    end
+  end
+end
+
+
+
+
+
